1001805194fa425d02320ece5324ccdbc64f51d9
Uday PG <pguday4@gmail.com>
2023-09-09 10:21:32 +0530

    Wierd Algorithms

---
+++ Introductory_Problems/WierdAlgorithm.java
@@ -0,0 +1,20 @@
+// package Introductory_Problems;
+import java.util.Scanner;
+class WierdAlgorithm{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        while(n > 1){
+            if(n % 2 == 0){
+                System.out.print(n+" ");
+                n /=2;
+            }
+            else{
+                System.out.print(n + " ");
+                n = n * 3 + 1;
+            }
+            
+        }
+        System.out.print(n);
+    }
+}
