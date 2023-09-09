61a42ca12ee5d1b9b8eb02ded428d4bf9e297f2d
Uday PG <pguday4@gmail.com>
2023-09-09 11:40:49 +0530

    Repetitions

---
+++ Introductory_Problems/Repetitions.java
@@ -0,0 +1,21 @@
+package Introductory_Problems;
+
+import java.util.Scanner;
+public class Repetitions
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int n = str.length();
+		int count = 1, max = 1;
+		for(int i = 1; i < n; i++){
+		  if(str.charAt(i) == str.charAt(i - 1)){
+		    count++;
+		    max = Math.max(max , count);
+		  }
+		  else count = 1;
+		}
+		System.out.println(max);
+		
+	}		
+}
